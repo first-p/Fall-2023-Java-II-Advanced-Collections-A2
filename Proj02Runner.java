68cbaab931008de0b4dc4bb064f2eaa2026a8985
first-p <98608650+first-p@users.noreply.github.com>
2023-10-29 23:56:00 -0500

    adding skeleton code for Proj02Runner class

--- Proj02Runner.java
+++ Proj02Runner.java
@@ -1,2 +1,21 @@
-public class Proj02Runner {
-}
+// File: Proj02Runner.java
+
+import java.util.*;
+
+public class Proj02Runner implements Comparable<Proj02Runner> {
+
+    // Instance variable to store the value
+    private int value;
+
+    // Default constructor
+    public Proj02Runner() {
+        System.out.println("I certify that this program is my own work\n" +
+                "and is not the work of others. I agree not\n" +
+                "to share my solution with others.\n" +
+                " Fred Butoma\n" );
+    }
+
+    // Overloaded constructor that takes an integer parameter
+    // Getter method for the value
+    // Implement the compareTo method of the Comparable interface
+    // Override the toString method to provide a meaningful string representation
